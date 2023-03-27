166db5accbc757b85c527d08d0523f335a3b8cd7
yangk <yangk@goodrain.com>
2023-03-27 22:01:55 +0800

    feat: support opentelemetry agent3

--- buildpacks/jvm-common/opt/opentelemetry.sh
+++ buildpacks/jvm-common/opt/opentelemetry.sh
@@ -5,5 +5,5 @@ OTEL_EXPORTER_OTLP_ENDPOINT=${OTEL_EXPORTER_OTLP_ENDPOINT:-http://tempo:4317}
 OTEL_RESOURCE_ATTRIBUTES=${OTEL_RESOURCE_ATTRIBUTES:-service.name=${OTEL_SERVICE_NAME},k8s_app=${K8S_APP}}
 OTEL_AGENT_VERSION=${OTEL_AGENT_VERSION:-v1.24.0}
 OTEL_AGENT_PATH=/app/agent
-
+echo "-----------------------------执行了---------------------------------------------------"
 export JAVA_OPTS="$JAVA_OPTS -javaagent:${OTEL_AGENT_PATH}/opentelemetry-javaagent-${OTEL_AGENT_VERSION}.jar"
